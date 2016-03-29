8bd2fd4414c7da0e69d755571a8102a285917c10
Charlene Pe <charlenepe@alumni.ateneo.edu>
2016-03-29 15:46:21 -0700

    localized

--- app/app.js
+++ app/app.js
@@ -34,6 +34,30 @@ angular
           }
         } 
       })
+
+      .state('profile', {
+      url: '/profile',
+      controller: 'ProfileCtrl as profileCtrl',
+      templateUrl: 'users/profile.html',
+
+      resolve: {
+        auth: function($state, Users, Auth){
+          return Auth.$requireAuth().catch(function(){
+            $state.go('home');
+          });
+        },
+        profile: function(Users, Auth){
+          return Auth.$requireAuth().then(function(auth){
+            return Users.getProfile(auth.uid).$loaded();
+          });
+        }
+      }
+    })  
+
+
+
+
+
       .state('register', {
         url: '/register',
         controller: 'AuthCtrl as authCtrl',
@@ -48,6 +72,9 @@ angular
           }
         }   
     });
+
+
+
 
     $urlRouterProvider.otherwise('/');
   })
--- app/users/users.service.js
+++ app/users/users.service.js
@@ -1,9 +1,3 @@
-angular.module('angularfireSlackApp')
-	.factory('Users', function($firebaseArray, $firebaseObject, FirebaseUrl){
-		var Users = {};
-		return Users;
-});
-
 
 angular.module('angularfireSlackApp')
   .factory('Users', function($firebaseArray, $firebaseObject, FirebaseUrl){
@@ -12,20 +6,22 @@ angular.module('angularfireSlackApp')
 
     var Users = {};
 
-    return Users;
-  });
-
-var Users = {
+    var Users = {
   getProfile: function(uid){
     return $firebaseObject(usersRef.child(uid));
   },
 
   getGravatar: function(uid){
-  	return '//www.gravatar.com/avatar/' + users.$getRecord(uid).emailHash;
+    return '//www.gravatar.com/avatar/' + users.$getRecord(uid).emailHash;
   },
 
   getDisplayName: function(uid){
     return users.$getRecord(uid).displayName;
   },
   all: users
-};
+
+
+  };
+
+    return Users;
+});

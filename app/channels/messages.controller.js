be9c4920223a3c2cb2789400aa7902580ff4ba20
Charlene Pe <charlenepe@alumni.ateneo.edu>
2016-03-30 01:59:14 -0700

    all working

--- app/channels/messages.controller.js
+++ app/channels/messages.controller.js
@@ -2,20 +2,20 @@ angular.module('angularfireSlackApp')
   .controller('MessagesCtrl', function(profile, channelName, messages){
     var messagesCtrl = this;
 
-	messagesCtrl.messages = messages;
-	messagesCtrl.channelName = channelName;
+    messagesCtrl.messages = messages;
+    messagesCtrl.channelName = channelName;
     messagesCtrl.message = '';
+	messagesCtrl.sendMessage = function (){
+	  if(messagesCtrl.message.length > 0){
+	    messagesCtrl.messages.$add({
+	      uid: profile.$id,
+	      body: messagesCtrl.message,
+	      timestamp: Firebase.ServerValue.TIMESTAMP
+	    }).then(function (){
+	      messagesCtrl.message = '';
+	    });
+	  }
+	};
 
-    messagesCtrl.sendMessage = function (){
-  if(messagesCtrl.message.length > 0){
-    messagesCtrl.messages.$add({
-      uid: profile.$id,
-      body: messagesCtrl.message,
-      timestamp: Firebase.ServerValue.TIMESTAMP
-    }).then(function (){
-      messagesCtrl.message = '';
-    });
-  }
-};
-
+    
   });
